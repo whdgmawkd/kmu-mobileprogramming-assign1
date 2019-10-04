e96392f31458c7bdbc0846ab0cf7e9d612956945
parkjongheum <whdgmawkd@naver.com>
2019-10-04 23:50:55 +0900

    LoginActivity: use database

--- app/src/main/java/com/test/mobileprogrammingassign1/LoginActivity.java
+++ app/src/main/java/com/test/mobileprogrammingassign1/LoginActivity.java
@@ -1,22 +1,38 @@
 package com.test.mobileprogrammingassign1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.test.mobileprogrammingassign1.models.Item;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button bLogin;
     private Button bRegister;
 
+    private EditText username;
+    private EditText password;
+
+    private AppDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        database = Room.databaseBuilder(this, AppDatabase.class, "mydb")
+                .build();
+
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
 
         bLogin = findViewById(R.id.button_login);
         bRegister = findViewById(R.id.button_register);
@@ -24,7 +40,19 @@ public class LoginActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(username.getText())){
+                    // empty field
+                    username.setError(getResources().getString(R.string.error_username_empty));
+                    return;
+                }
+                // query info
+                Item userinfo = database.getItemDAO().getItemByLoginInfo(username.getText().toString(), password.getText().toString());
+                if(userinfo == null){
+                    // TODO: 10/4/19 Show Error dialog
+                    Toast.makeText(getApplicationContext(),"Wrong Username or Password!", Toast.LENGTH_LONG).show();
+                } else {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
             }
         });
         bRegister.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +61,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
-
     }
 }
