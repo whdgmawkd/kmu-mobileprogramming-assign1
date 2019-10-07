ed06a9eb58e0d0ade0ab2d50d1ae3950e29095f5
parkjongheum <whdgmawkd@naver.com>
2019-10-07 23:58:01 +0900

    MainActivity: add option menu with refresh action

--- app/src/main/java/com/test/mobileprogrammingassign1/MainActivity.java
+++ app/src/main/java/com/test/mobileprogrammingassign1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.test.mobileprogrammingassign1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
